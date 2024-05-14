3b4da7ea0738568e4930238056f89789b70dbdec
Tharmarajan Punniamoorthy <tharma28@yahoo.com>
2024-05-13 21:45:38 -0400

    lookup data of martial status, gender

--- src/app/questionnaire/questionnaire-form.component.ts
+++ src/app/questionnaire/questionnaire-form.component.ts
@@ -17,7 +17,18 @@ export class questionnaireFormComponent {
     });
   } 
  
-  
+ 
+  genders :any = [
+    {id: '1', value:'Male'},
+    {id: '2', value:'Female'},
+    {id: '3', value:'Other'}
+    ];
+
+  martialStatus :any = [
+    {id: '1', value:'Married'},
+    {id: '2', value:'Single'},
+    {id: '3', value:'Divorced'}
+    ];
   submitted = false;
 
   onSubmit() { this.submitted = true; }
